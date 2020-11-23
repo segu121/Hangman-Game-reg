3b079e1c8ce241593dc35cac5576f52d3628bd9a
segu121 <slowik.sebastian1@gmail.com>
2020-11-23 22:23:16 +0100

    Dodaj pliki projektów.

---
+++ Hangman Game reg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hangman_Game_reg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
